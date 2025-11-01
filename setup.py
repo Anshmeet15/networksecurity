b16470144bbce30f052b10fd185c7619c63cc5aa
Anshmeet15 <anshmeetkaur41@gmail.com>
2025-11-01 19:28:12 +0530

    Setup.py file ready

--- setup.py
+++ setup.py
@@ -1,1 +1,68 @@
-# for packing code 
+# # for packing code 
+# from setuptools import find_packages,setup 
+# from typing import List
+
+# def get_requirements() -> List[str]:
+#     """
+#     This function reads the 'requirements.txt' file
+#     and returns a list of required packages.
+#     """
+#     requirement_lst:List[str]=[]
+
+#     try:
+#         with open('requirements.txt','r') as file:
+#             # read lines from files
+#             lines=file.readlines()
+#             # process each line
+#             for line in lines:
+#                   requirement=line.strip()
+#                 #   ignore emplty line and -e .
+#             if requirement and requirement!='-e .':
+#                 requirement_lst.append(requirement)
+#     except FileNotFoundError:
+#             print("requirements.txt file not found.")
+
+#     return requirement_lst
+# setup(
+#      name="NetworkSecurity",
+#      version="0.01",
+#      author="Anshmeet kaur",
+#      author_email="anshmeetkaur41@gmail.com",
+#      packages=find_packages(),
+#      install_requires=get_requirements()
+# )     
+
+
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """
+    This function reads the 'requirements.txt' file
+    and returns a list of required packages.
+    """
+    requirement_lst: List[str] = []
+
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                # ignore empty lines and '-e .'
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    return requirement_lst
+
+
+setup(
+    name="NetworkSecurity",
+    version="0.01",
+    author="Anshmeet Kaur",
+    author_email="anshmeetkaur41@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
